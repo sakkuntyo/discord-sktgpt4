9038ddd7945707ad881989b6350d2fce088623f2
Daisuke Sasaki <20591351+sakkuntyo@users.noreply.github.com>
2023-04-09 17:49:25 +0900

    inivisible

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ const {
   TextInputBuilder,
   TextInputStyle
 } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({ intents: [GatewayIntentBits.Guilds] ,presence: {status: "invisible"}});
 const axios = require("axios");
 
 client.on('ready', async () => {
